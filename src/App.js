a7b95c3a2bbc69b915b06cb8933944fe3aa0fde1
Nadia-si <nasimbi2011@gmail.com>
2023-07-03 20:49:35 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import dictionary from "./dictionary.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={dictionary}
-          className="App-image img-fluid"
-          alt="dictionary"
-        />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={dictionary}
+            className="App-image img-fluid"
+            alt="dictionary"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Nadia Simbi</small>
+        </footer>
+      </div>
     </div>
   );
 }
